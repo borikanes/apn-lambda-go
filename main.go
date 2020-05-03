a1853746e34f1a7335db7af7a2ad53a9bc9e6a0d
borikanes <ooludemi@terpmail.umd.edu>
2020-05-03 02:46:43 -0400

    trying to handle errors

--- main.go
+++ main.go
@@ -13,18 +13,22 @@ import (
         "log"
         "net/http"
         "os"
-        "strconv"
         "time"
 
 )
 
 func init() {
-    fmt.Printf("============================INIT RAN!!!!!============================")
+    log.Printf("Lambda loaded into memory")
+    var err error
+    jwtToken, err = generateJWTToken(os.Getenv("PRIVATE_KEY_FILE_NAME"))
+    if err != nil {
+        fmt.Printf("Error generating request initially => %s", err.Error())
+    }
 }
 
 var jwtToken = ""
 // curl command to make request to APN
-// curl -v -d '{"aps":{"alert":"hello"}}' -H "apns-topic: me.borikanes.SongUpdaterQA" -H "authorization: bearer " --http2 https://api.development.push.apple.com/3/device/<device_token>
+// curl -v -d '{"aps":{"alert":"hello"}}' -H "apns-topic: <bundle_id>" -H "authorization: bearer " --http2 https://api.development.push.apple.com/3/device/<device_token>
 
 const (
     sandboxBaseURL = "https://api.development.push.apple.com/"
@@ -33,8 +37,9 @@ const (
 
 // EventPayload represents the structure of the json response
 type EventPayload struct {
-        Message string `json:"message"`
+        Message string         `json:"message"`
         DeviceToken string     `json:"deviceToken"`
+        BundleID string        `json:"bundleID"`
 }
 
 // APSPayload is the Top level aps json object
@@ -52,19 +57,46 @@ type APSBodyPayload struct {
 type LambdaResponse struct {
     Base64Encoded bool `json:"isBase64Encoded"`
     Headers map[string]string `json:"headers"`
-    Message string `json:"body"`
+    Message string `json:"message"`
     StatusCode string `json:"statusCode"`
 }
 
+// // LambdaErrorResponse is the representation of an error - https://medium.com/@sgarcez/error-handling-with-api-gateway-and-go-lambda-functions-fe0e10808732
+// type LambdaErrorResponse struct {
+// 	statusCode    string
+// 	message string
+// }
+//
+// func (e LambdaErrorResponse) Error() string {
+// 	b, err := json.Marshal(e)
+// 	if err != nil {
+// 		log.Println("cannot marshal Error:", e)
+// 		panic(err)
+// 	}
+// 	return string(b[:])
+// }
+//
+// // MarshalJSON encoding
+// func (e LambdaErrorResponse) MarshalJSON() ([]byte, error) {
+// 	return json.Marshal(&struct {
+// 		StatusCode string `json:"statusCode"`
+// 		Message  string `json:"message"`
+// 	}{
+// 		StatusCode: e.statusCode,
+// 		Message:  e.message,
+// 	})
+// }
+
 // HandleLambdaEvent is the lambda event lambda
-func HandleLambdaEvent(event EventPayload) (LambdaResponse, error) {
+func HandleLambdaEvent(event EventPayload) (LambdaResponse, error/**LambdaErrorResponse*/) {
     log.Printf("Message: %s || Device Token: %s", event.Message, event.DeviceToken)
 
     apsBodyPayload := APSBodyPayload{Alert: event.Message, Sound: "default"}
-    apnRequest, err := formRequestObject(APSPayload {Aps: apsBodyPayload}, event.DeviceToken)
+    apnRequest, err := formRequestObject(APSPayload {Aps: apsBodyPayload}, event.DeviceToken, event.BundleID)
     if err != nil {
         log.Printf("Error forming request object, see below for more info")
-        return LambdaResponse{Message: "Error forming request object", StatusCode: "500", Base64Encoded: false, Headers: map[string]string{}}, nil
+        // error := &LambdaErrorResponse{statusCode: "500", message: "Error forming request object"}
+        return LambdaResponse{}, fmt.Errorf("Error forming request object. code: 500")
     }
 
     // Send request
@@ -72,20 +104,20 @@ func HandleLambdaEvent(event EventPayload) (LambdaResponse, error) {
 
     // Token expired, refresh and resend request
     if statusCode == http.StatusForbidden {
-        log.Printf("Got a 403.....Refreshing token now. Error => %s", err.Error())
+        log.Printf("Got a 403.....Refreshing token now.")
         var generateTokenError error // Need error object because generateJWTToken returns error
         jwtToken, generateTokenError = generateJWTToken(os.Getenv("PRIVATE_KEY_FILE_NAME")) // Reassign refreshed token to global jwtToken variable
         // Issue with token generation
         if generateTokenError != nil {
             log.Printf("Error in generating token when request to APN is 403. Error => %s", generateTokenError.Error())
-            return LambdaResponse{Message: "Error generating token when trying to refresh", StatusCode: "500", Base64Encoded: false, Headers: map[string]string{}}, nil
+            return LambdaResponse{}, fmt.Errorf("Error generating token when trying to refresh. code: 500")
         }
         // Resend request and reassign statusCode and err.
         statusCode, err = sendRequest(apnRequest)
     }
     // If statusCode is not 200 just return error to client.
     if statusCode != http.StatusOK {
-        return LambdaResponse{Message: fmt.Sprintf("Error when sending request. Status Code %d", statusCode), StatusCode: strconv.Itoa(statusCode), Base64Encoded: false, Headers: map[string]string{}}, err
+        return LambdaResponse{}, fmt.Errorf("Error generating token when trying to refresh. code: %d", statusCode)
     }
 
     // if response is a 200
@@ -132,12 +164,12 @@ func generateJWTToken(privateKeyPath string) (string, error) {
       log.Printf("Error when trying to jwt Sign. Error => %s", error.Error())
       return "",error
     }
-    fmt.Printf(string(token))
+    fmt.Println(string(token))
 
     return string(token),nil
 }
 
-func formRequestObject(payload APSPayload, deviceToken string) (*http.Request, error) {
+func formRequestObject(payload APSPayload, deviceToken string, bundleID string) (*http.Request, error) {
     // convert to json
     requestBody, err := json.Marshal(payload)
     if err != nil {
@@ -146,7 +178,7 @@ func formRequestObject(payload APSPayload, deviceToken string) (*http.Request, e
 
     // get endpoint based on environment
     baseEndpoint := productionBaseURL
-    if os.Getenv("ENV") != "QA" {
+    if os.Getenv("ENV") == "QA" {
         baseEndpoint = sandboxBaseURL
     }
 
@@ -159,7 +191,7 @@ func formRequestObject(payload APSPayload, deviceToken string) (*http.Request, e
 
     // set headers
     request.Header.Set("Content-Type", "application/json")
-    request.Header.Set("apns-topic", os.Getenv("BUNDLE_ID"))
+    request.Header.Set("apns-topic", bundleID)
     // request.Header.Set("authorization", "Bearer "+jwtToken)
 
     return request, nil
@@ -171,46 +203,22 @@ func formRequestObject(payload APSPayload, deviceToken string) (*http.Request, e
 // 404 - {"reason":"BadPath"}
 func sendRequest(request *http.Request) (int, error) {
     request.Header.Set("authorization", "Bearer "+jwtToken)
-
     client := &http.Client {}
     response, err := client.Do(request)
     if err != nil {
         return 500, err
     }
+    // bodyBytes, _ := ioutil.ReadAll(response.Body)
+    // log.Println(string(bodyBytes))
     defer response.Body.Close()
-    // 403: Token probably expired or is somehow invalid or nil, generate token
-    // if response.StatusCode == http.StatusForbidden {
-    //     log.Printf("403 Error....Trying to re-generate token")
-    //     var generateTokenError error
-    //     jwtToken, generateTokenError = generateJWTToken(os.Getenv("PRIVATE_KEY_FILE_NAME"))
-    //     if generateTokenError != nil {
-    //         log.Printf("Error in generating token when request to APN is 403. Error => %s", generateTokenError.Error())
-    //         return response.StatusCode, generateTokenError
-    //     }
-    //
-    //     log.Printf("Successfully refreshed token")
-    // } else if response.StatusCode != http.StatusOK {    // Any other error
-    //
-    //     // maybe do some kind of retry mechanism here?
-    //     log.Printf("Received an error other than 200. Response code => %d", response.StatusCode)
-    //     return response.StatusCode,fmt.Errorf("Received an error other than 200. Response code => %d", response.StatusCode)
-    // }
+
     log.Printf("Status Code when sending request => Status Code: %d", response.StatusCode)
-    // if response.StatusCode != http.StatusOK {
-    //     return response.StatusCode,fmt.Errorf("Received a %d", response.StatusCode)
-    // }
     return response.StatusCode, nil
 }
 
 func main() {
-    log.Printf("Lambda loaded into memory")
-    var err error
-    jwtToken, err = generateJWTToken(os.Getenv("PRIVATE_KEY_FILE_NAME"))
-    if err != nil {
-        fmt.Printf("Error generating request initially => %s", err.Error())
-    }
     // Local development
-    // lambdaResponse, error := HandleLambdaEvent(EventPayload{Message: "MESSAGE!!!", DeviceToken: "1234567890"})
+    // lambdaResponse, error := HandleLambdaEvent(EventPayload{Message: "MESSAGE!!!", DeviceToken: "1234567890", BundleID: ""})
     // if error != nil {
     //     fmt.Println(error)
     // }
